1ca69ce05dc166756c950b9fae013637193b778e
Mei Huang <mei.h.hmi@gmail.com>
2023-04-12 11:42:44 +0800

    build(env): 基礎環境配置

---
+++ webpack.dev.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname,
+    filename: "./release/bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./release"),
+    },
+    open: true,
+    port: 9000,
+  },
+};
